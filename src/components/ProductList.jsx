d5cc08cb35c901e4507f6ebde255e4234d5d6a22
yns032 <ya.aslan.44@gmail.com>
2023-07-24 23:09:01 +0300

    add

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,35 @@
+// ProductList.js
+import React from "react";
+import { useCart } from "../context/BasketContext";
+
+const products = [
+  { id: 1, name: "Ürün 1", price: 10 },
+  { id: 2, name: "Ürün 2", price: 20 },
+  { id: 3, name: "Ürün 3", price: 30 },
+];
+
+const ProductList = () => {
+  const { dispatch } = useCart();
+
+  const handleAddToCart = (product) => {
+    dispatch({ type: "ADD_TO_CART", payload: product });
+  };
+
+  return (
+    <div>
+      <h2>Ürün Listesi</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.price} TL{" "}
+            <button onClick={() => handleAddToCart(product)}>
+              Sepete Ekle
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;
