e9c67e2ff57330a6de3d9a0606c332d85b92b15b
yns032 <ya.aslan.44@gmail.com>
2023-07-24 23:50:41 +0300

    ADD_TO_CART and REMOVE_FROM_CART condition changed

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -1,14 +1,43 @@
-// cartReducer.js
 const cartReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
-      console.log(action);
-      return state;
+      const { id, name, price } = action.payload;
+      const checkItem = state.cartItems.find((item) => item.id === id);
+      if (checkItem) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((item) =>
+            item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, { id, name, price, quantity: 1 }],
+        };
+      }
+
     case "REMOVE_FROM_CART":
-      return {
-        ...state,
-        cartItems: state.cartItems.filter((item) => item.id !== action.payload),
-      };
+      const deletedId = action.payload;
+      const checkDeleteItem = state.cartItems.find(
+        (item) => item.id === deletedId
+      );
+      if (checkDeleteItem.quantity > 1) {
+        return {
+          ...state,
+          cartItems: state.cartItems.map((item) =>
+            item.id === deletedId
+              ? { ...item, quantity: item.quantity - 1 }
+              : item
+          ),
+        };
+      } else {
+        return {
+          ...state,
+          cartItems: state.cartItems.filter((item) => item.id !== deletedId),
+        };
+      }
+
     default:
       return state;
   }
