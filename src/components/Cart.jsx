c1575e3fea45b633c11ec9c7d7723abe5653b4f1
yns032 <ya.aslan.44@gmail.com>
2023-07-24 23:36:48 +0300

    cart add

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,7 +6,21 @@ const Cart = () => {
   const { cart, dispatch } = useCart();
 
   const handleRemoveFromCart = (productId) => {
-    dispatch({ type: "REMOVE_FROM_CART", payload: productId });
+    debugger;
+    const existingItem = cart.cartItems.find((item) => item.id === productId);
+
+    if (existingItem) {
+      if (existingItem.quantity > 1) {
+        dispatch({ type: "REMOVE_FROM_CART", payload: productId });
+      }
+    }
+  };
+
+  const calculateTotalPrice = () => {
+    return cart.cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
   };
 
   return (
@@ -15,13 +29,14 @@ const Cart = () => {
       <ul>
         {cart.cartItems.map((item) => (
           <li key={item.id}>
-            {item.name} - {item.price} TL{" "}
+            {item.name} - {item.price} TL (x{item.quantity}){" "}
             <button onClick={() => handleRemoveFromCart(item.id)}>
               Sepetten Çıkar
             </button>
           </li>
         ))}
       </ul>
+      <p>Toplam Tutar: {calculateTotalPrice()} TL</p>
     </div>
   );
 };
