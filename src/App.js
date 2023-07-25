3f4f8b30673edf0f0434bc0904263b5654d23dea
yns032 <ya.aslan.44@gmail.com>
2023-07-25 22:36:29 +0300

    app add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Cart from "./components/Cart";
-import ProductList from "./components/ProductList";
 import { CartProvider } from "./context/BasketContext";
 import { Coloria, ColoriaProvider, useColoria } from "./context/ColoriaContext";
+import Layout from "./pages/Layout/Layout";
+import List from "./pages/List";
+import Sepet from "./components/sepet/Sepet";
+import Favori from "./components/favori/Favori";
+import Home from "./components/sepet/Home";
 
 function BasketList(params) {
   const { state, dispatch } = useColoria();
@@ -12,17 +18,19 @@ function BasketList(params) {
 function App() {
   return (
     <div className="App">
-      <ColoriaProvider>
-        <h1>Colorialar</h1>
-        <Coloria id="coloria1" />
-        <Coloria id="coloria2" />
-        <Coloria id="coloria3" />
-        <BasketList />
-      </ColoriaProvider>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <BrowserRouter>
+          <List />
+          <Routes>
+            <Route element={<Layout />} />
+            {/* <Route index element={<Home />} /> */}
+            <Route path="/sepet" element={<Sepet />} />
+            <Route path="/favori" element={<Favori />} />
+          </Routes>
+        </BrowserRouter>
+      </nav>
       <CartProvider>
-        <h1>Sepet Uygulaması</h1>
-        <ProductList />
-        <Cart />
+        <Home />
       </CartProvider>
     </div>
   );
